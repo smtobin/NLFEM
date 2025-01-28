bad47d800119ce1da939374b0f88623c5a2f3bf1
smtobin <smtobin@wisc.edu>
2025-01-27 20:54:09 -0500

    Added CMakeLists.txt and executable

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+int main(int argc, char **argv) 
+{
+    Eigen::Vector3d test_vec(0,0,0);
+    std::cout << test_vec << std::endl;
+    std::cout << "Hello!" << std::endl;
+}
