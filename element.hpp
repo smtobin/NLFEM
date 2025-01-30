edcad933a740ec74cfe17d3bb3c34072452f963e
smtobin <smtobin@wisc.edu>
2025-01-29 22:19:10 -0500

    Basic working linear FEM

--- element.hpp
+++ element.hpp
@@ -16,20 +16,6 @@ class QuadElement
 
     }
 
-    // evaluates the shape function matrix H at (x,y)
-    // Eigen::Matrix<double, 2, 8> H(double x, double y) const
-    // {
-    //     double h1 = (_x2[0] - x) * (_x4[1] - y) / (_x2[0] - _x1[0]) / (_x4[1] - _x1[1]);
-    //     double h2 = (x - _x1[0]) * (_x3[1] - y) / (_x2[0] - _x1[0]) / (_x3[1] - _x2[1]);
-    //     double h3 = (_x4[0] - x) * (y - _x2[1]) / (_x4[0] - _x3[0]) / (_x3[1] - _x2[1]);
-    //     double h4 = (x - _x3[0]) * (y - _x1[1]) / (_x4[0] - _x3[0]) / (_x4[1] - _x1[1]);
-
-    //     Eigen::Matrix<double, 2, 8> H_mat;
-    //     H_mat << h1, h2, h3, h4, 0,  0,  0,  0,
-    //              0,  0,  0,  0,  h1, h2, h3, h4;
-
-    //     return H_mat;
-    // }
     Eigen::Matrix<double, 2, 8> H(double r, double s) const
     {
         double h1 = 0.25 * (1 + r) * (1 + s);
@@ -38,32 +24,12 @@ class QuadElement
         double h4 = 0.25 * (1 + r) * (1 - s);
 
         Eigen::Matrix<double, 2, 8> H_mat;
-        H_mat << h1, h2, h3, h4, 0,  0,  0,  0,
-                    0,  0,  0,  0,  h1, h2, h3, h4;
+        H_mat << h1, 0,  h2, 0,  h3, 0,  h4, 0,
+                 0,  h1, 0,  h2, 0,  h3, 0,  h4;
 
         return H_mat;
     }
 
-    // evalutes the strain-displacement matrix B at (x,y)
-    // Eigen::Matrix<double, 3, 8> B(double x, double y) const
-    // {
-    //     double dh1_dx = -(_x4[1] - y) / (_x2[0] - _x1[0]) / (_x4[1] - _x1[1]);
-    //     double dh2_dx = (_x3[1] - y) / (_x2[0] - _x1[0]) / (_x3[1] - _x2[1]);
-    //     double dh3_dx = -(y - _x2[1]) / (_x4[0] - _x3[0]) / (_x3[1] - _x2[1]);
-    //     double dh4_dx = (y - _x1[1]) / (_x4[0] - _x3[0]) / (_x4[1] - _x1[1]);
-    
-    //     double dh1_dy = -(_x2[0] - x) / (_x2[0] - _x1[0]) / (_x4[1] - _x1[1]);
-    //     double dh2_dy = -(x - _x1[0]) / (_x2[0] - _x1[0]) / (_x3[1] - _x2[1]);
-    //     double dh3_dy = (_x4[0] - x) / (_x4[0] - _x3[0]) / (_x3[1] - _x2[1]);
-    //     double dh4_dy = (x - _x3[0]) / (_x4[0] - _x3[0]) / (_x4[1] - _x1[1]);
-    
-    //     Eigen::Matrix<double, 3, 8> B_mat;
-    //     B_mat << dh1_dx, dh2_dx, dh3_dx, dh4_dx, 0, 0, 0, 0,
-    //              0, 0, 0, 0, dh1_dy, dh2_dy, dh3_dy, dh4_dy,
-    //              dh1_dy, dh2_dy, dh3_dy, dh4_dy, dh1_dx, dh2_dx, dh3_dx, dh4_dx;
-
-    //     return B_mat;
-    // }
     Eigen::Matrix<double, 3, 8> B(double r, double s) const
     {
         Eigen::Vector4d dh_dr;
@@ -83,15 +49,16 @@ class QuadElement
 
         Eigen::Matrix<double, 3, 8> B_mat = Eigen::Matrix<double, 3, 8>::Zero();
         for (int i = 0; i < 4; i++)
-            B_mat(0,i) = J_inv(0,0)*dh_dr(i) + J_inv(0,1)*dh_ds(i);
-        
-        for (int i = 0; i < 4; i++)
-            B_mat(1,i+4) = J_inv(1,0)*dh_dr(i) + J_inv(1,1)*dh_ds(i);
+        {
+            B_mat(0,2*i) = J_inv(0,0)*dh_dr(i) + J_inv(0,1)*dh_ds(i);
+            B_mat(1,2*i+1) = J_inv(1,0)*dh_dr(i) + J_inv(1,1)*dh_ds(i);
+        }
+            
         
         for (int i = 0; i < 4; i++)
         {
-            B_mat(2,i) = B_mat(1,i+4);
-            B_mat(2,i+4) = B_mat(0,i);
+            B_mat(2,2*i) = B_mat(1,2*i+1);
+            B_mat(2,2*i+1) = B_mat(0,2*i);
         }
 
         return B_mat;
@@ -108,8 +75,6 @@ class QuadElement
         double dh2_ds = 0.25 * (1 - r);
         double dh3_ds = -0.25 * (1 - r);
         double dh4_ds = -0.25 * (1 + r);
-        const double d_dr = dh1_dr + dh2_dr + dh3_dr + dh4_dr;
-        const double d_ds = dh1_ds + dh2_ds + dh3_ds + dh4_ds;
 
         Eigen::Matrix2d J_mat;
         J_mat << dh1_dr*_x1[0] + dh2_dr*_x2[0] + dh3_dr*_x3[0] + dh4_dr*_x4[0],
@@ -142,9 +107,10 @@ class QuadElement
     {
         // elasticity matrix for plane strain
         Eigen::Matrix3d C_mat;
-        C_mat <<    _E / (1-_mu*_mu),      _E*_mu / (1-_mu*_mu),   0,
-                _E*_mu / (1-_mu*_mu),   _E / (1-_mu*_mu),      0,
-                0,                  0,                  0.5*_E*(1-_mu)/(1-_mu*_mu); 
+        C_mat << (1-_mu), _mu, 0,
+                 _mu, (1-_mu), 0,
+                 0, 0, 0.5*(1-2*_mu);
+        C_mat *= _E / ( (1+_mu) * (1-2*_mu)); 
         return C_mat;
     }
 
@@ -153,13 +119,13 @@ class QuadElement
         std::array<double, 2> gauss_pts({-1.0/std::sqrt(3), 1.0/std::sqrt(3)});
         Eigen::Matrix<double, 8, 8> K_mat = Eigen::Matrix<double, 8, 8>::Zero();
         const Eigen::Matrix3d C_mat = C();
-        std::cout << C_mat << std::endl;
         for (const auto& ri : gauss_pts)
         {
             for (const auto& sj : gauss_pts)
             {
                 const Eigen::Matrix2d J_mat = J(ri, sj);
                 const Eigen::Matrix<double, 3, 8> B_mat = B(ri, sj);
+                std::cout << "\nB(" << ri << "," << sj << "):\n" << B_mat << std::endl;
                 K_mat += B_mat.transpose() * C_mat * B_mat * J_mat.determinant();
             }
         }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include "element.hpp"
 
-int main(int argc, char **argv) 
+int main(int /* argc */, char** /* argv */) 
 {
     // define the material (given in the problem statement)
     const double E = 100;
@@ -16,7 +16,55 @@ int main(int argc, char **argv)
     const double density = 1;
     QuadElement element(x1, x2, x3, x4, density, E, mu);
 
-    std::cout << element.M() << std::endl;
-    std::cout << element.K() << std::endl;
+    Eigen::Matrix<double, 8, 8> K = element.K();
+    Eigen::Vector<double, 8> R({0, 0, 0, 0, 0, 0, 0, 0});
 
+    // prescribe displacements
+    //  u1 = 0.01
+    //  u2 = 0
+    //  u3 = 0
+    //  u4 = 0.01
+
+    // u1
+    K.row(0) = Eigen::Vector<double,8>({1,0,0,0,0,0,0,0});
+    for (int i = 0; i < 8; i++)
+    {
+        R(i) -= K(i,0)*0.01;
+    }
+    R(0) = 0.01;
+    K.col(0) = K.row(0);
+
+    // u2
+    K.row(2) = Eigen::Vector<double,8>({0,0,1,0,0,0,0,0});
+    R(2) = 0;
+    K.col(2) = K.row(2);
+
+    // u3
+    K.row(4) = Eigen::Vector<double,8>({0,0,0,0,1,0,0,0});
+    R(4) = 0;
+    K.col(4) = K.row(4);
+
+    // u4
+    K.row(6) = Eigen::Vector<double,8>({0,0,0,0,0,0,1,0});
+    R(1) -= K(1,6)*0.01;
+    R(3) -= K(3,6)*0.01;
+    R(5) -= K(5,6)*0.01;
+    R(7) -= K(7,6)*0.01;
+    R(6) = 0.01;
+    K.col(6) = K.row(6);
+
+    std::cout << "Modified K:\n" << K << std::endl;
+    std::cout << "Modified R:\n" << R << std::endl;
+
+    Eigen::Vector<double, 8> U = K.llt().solve(R);
+    std::cout << "\nK:\n" << element.K() << std::endl;
+    std::cout << "\nM:\n" << element.M() << std::endl;
+    std::cout << "\nU:\n" << U << std::endl;
+
+    //////
+
+    Eigen::Matrix<double, 3, 8> B = element.B(-0.577, -0.577);
+    // Eigen::Vector<double, 8> u_hat({0.5, 0, 0, 0, 0, 0, 0, 0});
+    std::cout << "\n" << B*U << std::endl;
+    std::cout << "\n" << element.C()*B*U << std::endl;
 }
